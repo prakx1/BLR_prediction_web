57c66235c905d9374b832f432ceac6b0f4fc42ad
prakx <prakx2050@gmail.com>
2021-07-13 07:23:10 +0530

    update server.js

--- server.py
+++ server.py
@@ -33,8 +33,8 @@ def predict_home_price():
 
 @app.route('/')
 def hell_world():
-    util.load_saved_artifacts()
     return render_template("app.html")
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
     app.run()
